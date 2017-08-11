ea20388c4e7a57ab22bbda588adaf6d0be898cc5
Quang Bao <quangbao1994@gmail.com>
2017-08-12 00:28:06 +0700

    Fix

--- app/controllers/post.server.controller.js
+++ app/controllers/post.server.controller.js
@@ -33,17 +33,24 @@ exports.loadFeeds = function (req, res) {
 };
 
 exports.loadNewerFeeds = function (req, res) {
-	var lastPostId = req.body.lastPostId || "0";
-	console.log('Last post id of feeds: ', lastPostId);
+	var page = parseInt(req.body.page) || PAGE;
+	var sortBy = req.body.sortBy || SORTBY;
 	console.log('Number of feeds: ', NUMOFPOST);
-	oid = mongoose.Types.ObjectId(lastPostId)
-	console.log(mongoose.Types.ObjectId.isValid(lastPostId));
-
-	Post.find({
-		id: {
-			$gt: oid
+	console.log('Sort by: ', sortBy);
+	console.log(req.body.groupId);
+	var query = {};
+	if (!!req.body.groupId) {
+		query = {
+			group_id: req.body.groupId,
+			created_time: req.body.created_time
 		}
-	}).limit(100).sort({
+		console.log(query);
+	}
+	Post.find({
+		created_time: {
+			$gt: query.created_time
+		}
+	}).limit(50).sort({
 		"created_time": -1
 	}).exec(function (err, result) {
 		if (err) console.log('Error', err);
